416b7d91bf790d2992ac647454ebb9c56eed2622
Trina Castillo <trina.castillo@expeditors.com>
2017-06-21 11:49:05 -0700

    tutorial complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from "@angular/router";
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostsService } from "./posts.service";
+
+// Define the routes
+const ROUTES = [
+    {
+        path: '',
+        redirectTo: 'posts',
+        pathMatch: 'full'
+    },
+    {
+        path: 'posts',
+        component: PostsComponent
+    }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PostsComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(ROUTES) // Add routes to the app
+    ],
+    providers: [PostsService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
